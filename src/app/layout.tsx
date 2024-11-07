8b78d2d73cfcf626f287dcd75e09fde6a60c7e70
gruzam0615 <gruzam0615@gmail.com>
2024-11-07 18:26:51 +0900

    header navbar 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,15 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import TopNav from "./fragments/topnav";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+  src: "./utils/fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
+  src: "./utils/fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
@@ -24,10 +25,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <TopNav/>
         {children}
       </body>
     </html>
