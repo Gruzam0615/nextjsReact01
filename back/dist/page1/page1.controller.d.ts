dff65a6f57c71ef5ffbe8251ce4f97074231024d
gruzam0615 <gruzam0615@gmail.com>
2024-12-02 21:58:10 +0900

    back/dist 제거

--- back/dist/page1/page1.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { HttpService } from "@nestjs/axios";
-export declare class Page1Controller {
-    private readonly httpService;
-    constructor(httpService: HttpService);
-    findAll(): any;
-    findAll2(): any;
-    findAll3(server_name: string): Promise<any>;
-}
