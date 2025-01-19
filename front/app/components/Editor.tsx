358dd495df444497205246e1fc9dbdf63a7bbc28
gruzam0615 <gruzam0615@gmail.com>
2025-01-20 01:03:19 +0900

    /page2/write 페이지에 quill editor 적용

---
+++ front/app/components/Editor.tsx
@@ -0,0 +1,94 @@
+import { useMemo } from "react";
+import ReactQuill, { Quill } from "react-quill-new";
+import "react-quill-new/dist/quill.snow.css";
+
+const EditorComponent = ({ readOnly, defaultValue, onTextChange, onChangeSelection, ref }: any) => {
+
+  /**
+   * Backend가 없는 상태에서 Frontend 시연을 위한 Rich Editor 이미지 첨부 파일 처리기
+   * @param fileURL 
+   * @returns 
+   */
+  const LocalImageUrlHandler = (fileURL: string) => {
+    // console.log(`fileURL: ${fileURL}`);
+    const ImageBlot: any = Quill.import("formats/image");
+    ImageBlot.sanitize = function (fileURL: string) {
+      return fileURL
+    }
+    return ImageBlot.sanitize(fileURL);
+  }
+
+  const ImageHandler = () => {
+    const maxFileCount = 10;
+    const input = document.createElement("input");
+    input.setAttribute("type", "file");
+    input.setAttribute("accept", "image/*")
+    input.setAttribute("multiple", "");
+    input.click();
+
+    input.addEventListener("change", async (e) => {
+      const targetFiles = (e.target as HTMLInputElement).files as FileList;
+      const targetFilesArray = Array.from(targetFiles);
+
+      if (targetFilesArray.length > maxFileCount) {
+        alert(`첨부 가능한 파일개수는 ${maxFileCount} 개 입니다.`)
+        return;
+      } else {
+        const selectedFiles: string[] = targetFilesArray.map((file) => { return URL.createObjectURL(file); });
+        const selectedFilesNameList: string[] = targetFilesArray.map((file) => { return file.name; });
+        const editor = await ref.current.getEditor();
+        const range = await editor.getSelection();
+
+        selectedFiles.forEach((file: string, index: number) => {
+          const name = LocalImageUrlHandler(file);
+          // console.log(`name: ${name}`);
+          editor.insertEmbed(range.index + index, "image", name);
+        })
+
+      }
+    })
+  }
+
+  const modules = useMemo(() => {
+    return {
+      toolbar: {
+        container: [
+          [{ "header": [1, 2, 3, 4, 5, false] }],
+          ["bold", "italic", "underline", "strike", "blockquote"],
+          [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }],
+          ["link", "image"],
+        ],
+        handlers: {
+          image: ImageHandler,
+        },
+      }
+    }
+  }, []);
+
+  const formats = [
+    'header',
+    'bold', 'italic', 'underline', 'strike', 'blockquote',
+    'list',
+    'link', 'image'
+  ];
+
+  return (
+    <>
+      <ReactQuill
+        theme="snow"
+        readOnly={readOnly}
+        value={defaultValue}
+        onChange={onTextChange}
+        onChangeSelection={onChangeSelection}
+        modules={modules}
+        formats={formats}
+        ref={ref}
+        style={{
+          height: "850px",
+        }}
+      />
+    </>
+  );
+}
+
+export default EditorComponent;
--- front/app/page2/write/page.tsx
+++ front/app/page2/write/page.tsx
@@ -1,14 +1,18 @@
 "use client";
 
-import { Button, Label, TextInput } from "flowbite-react";
-import { content } from "flowbite-react/tailwind";
-import { useState } from "react";
+import React, { memo, useRef, useState } from "react";
+import { Button, FileInput, Label, TextInput } from "flowbite-react";
+import "@/app/style/Write.css";
+import EditorComponent from "@/app/components/Editor";
 
 export default function WritePost() {
-    const [ title, setTitle ] = useState("");
-    const [ author, setAuthor ] = useState("");
-    const [ date, setDate ] = useState("");
-    const [ content, setContent ] = useState("");
+    const [title, setTitle] = useState("");
+    const [author, setAuthor] = useState("");
+    const [date, setDate] = useState("");
+    const [content, setContent] = useState("");
+    const [fileInputNameList, setFileInputNameList] = useState<string[]>([]);
+    const [fileInput, setFileInput] = useState<string[]>([]);
+    const fileInputRef = useRef<HTMLInputElement>(null);
 
     const onChangeTitle = (e: any) => {
         setTitle(e.target.value);
@@ -26,14 +30,51 @@ export default function WritePost() {
         setContent(e.target.value);
     }
 
+    const fileInputLabelClick = () => {
+        fileInputRef?.current?.click();
+    }
+
+    const onChangeFileInput = (e: React.ChangeEvent) => {
+        const targetFiles = (e.target as HTMLInputElement).files as FileList;
+        const targetFilesArray = Array.from(targetFiles);
+        const selectedFiles: string[] = targetFilesArray.map((file) => { return URL.createObjectURL(file); });
+        const selectedFilesNameList: string[] = targetFilesArray.map((file) => { return file.name; });
+        setFileInput((prev) => prev.concat(selectedFiles));
+        setFileInputNameList((prev) => prev.concat(selectedFilesNameList));
+    }
+
+    // const onChangeFileInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    //     const files = e.target.files!
+    //     if (!files[0]) return
+    //     if ((fileInput.length + files.length) > 10) {
+    //         return alert('최대 10개 사진만 첨부할 수 있습니다.')
+    //     }
+    //     const readAndPreview = (file: any) => {
+    //         if (/\.(jpe?g|png|gif)$/i.test(file.name)) {
+    //             const reader = new FileReader()
+    //             reader.onload = () => setFileInput(prev => [...prev, reader.result as string])
+    //             reader.readAsDataURL(file)
+    //         }
+    //     }
+    //     if (files) {
+    //         [].forEach.call(files, readAndPreview)
+    //     }
+    //     console.log(fileInput);
+    // }
+
     const doPost = () => {
         console.log({
             "title": title,
             "author": author,
             "date": date,
-            "content": content
+            "content": content,
+            "fileInput": fileInput,
         })
     }
+
+    const [ range, setRange ] = useState();
+    const [ readOnly, setReadOnly ] = useState<boolean>(false);
+    const quillRef = useRef(null);
 
     return (
         <div className="flex flex-col">
@@ -41,47 +82,77 @@ export default function WritePost() {
                 <h5 className="text-2xl">신규 작성</h5>
             </div>
             <form>
-            <div className="flex flex-col">
-                <div className="flex flex-row h-12 text-end">
-                    <div className="w-2/12 self-center">
-                        <Label htmlFor="title" value="title: " />
+                <div className="flex flex-col">
+                    <div className="flex flex-row h-12 text-end">
+                        <div className="w-2/12 self-center">
+                            <Label htmlFor="title" value="제목: " />
+                        </div>
+                        <div className="w-9/12 self-center">
+                            <TextInput id="title" type="text" sizing="sm" value={title} onChange={onChangeTitle} required />
+                        </div>
                     </div>
-                    <div className="w-9/12 self-center">
-                        <TextInput id="title" type="text" sizing="sm" value={title} onChange={onChangeTitle} required />
+                    <div className="flex flex-row h-12 text-end">
+                        <div className="w-2/12 self-center">
+                            <Label htmlFor="author" value="작성자: " />
+                        </div>
+                        <div className="w-9/12 self-center">
+                            <TextInput id="author" type="text" sizing="sm" value={author} onChange={onChangeAuthor} required />
+                        </div>
+                    </div>
+                    <div className="flex flex-row h-12 text-end">
+                        <div className="w-2/12 self-center">
+                            <Label htmlFor="date" value="작성일: " />
+                        </div>
+                        <div className="w-9/12 self-center">
+                            <TextInput id="date" type="datetime-local" value={date} onChange={onChangeDate} sizing="sm" />
+                        </div>
+                    </div>
+                    <div className="flex flex-row h-[900px]">
+                        <div className="w-2/12 text-end">
+                            <Label htmlFor="content" value="본문: " />
+                        </div>
+                        <div className="w-9/12">
+                            {/* <TextInput id="content" type="text" sizing="sm" value={content} onChange={onChangeContent} /> */}
+                            <EditorComponent
+
+                                readOnly={readOnly}
+                                defaultValue={content}
+                                onChangeSelection={setRange}
+                                onTextChange={setContent}
+                                ref={quillRef}
+                            />
+                        </div>
+                    </div>
+                    <div className="flex flex-row h-12">
+                        <div className="w-2/12 text-end self-center">
+                            <Label htmlFor="fileInput" value="첨부 파일: " />
+                            <input id="fileInput" className="fileInput" type="file" accept="image/*" ref={fileInputRef} multiple onChange={onChangeFileInput}/>
+                        </div>
+                        <div className="w-9/12 text-center self-center">
+                            <Label value="첨부 파일 목록" />
+                        </div>
+                    </div>
+                    {
+                        //  fileInput.map((file, index) => (
+                        //     <img key={index} src={file} width="280" height="160" alt={`image_${index}`} />
+                        // ))
+                        fileInputNameList.map((file, index) => (
+                            <div key={index} className="flex flex-row h-12" onClick={fileInputLabelClick}>
+                                <div className="w-2/12"></div>
+                                <div className="w-9/12 text-center self-center">
+                                    <Label value={file} />
+                                </div>
+                            </div>
+                        ))
+                    }
+                    <div className="flex flex-row h-12 justify-end">
+                        <div className="w-9/12"></div>
+                        <div className="w-2/12">
+                            <Button color="gray" onClick={doPost}>작성</Button>
+                        </div>
                     </div>
                 </div>
-                <div className="flex flex-row h-12 text-end">
-                    <div className="w-2/12 self-center">
-                        <Label htmlFor="author" value="author: " />
-                    </div>
-                    <div className="w-9/12 self-center">
-                        <TextInput id="author" type="text" sizing="sm" value={author} onChange={onChangeAuthor} required/>
-                    </div>
-                </div>
-                <div className="flex flex-row h-12 text-end">
-                    <div className="w-2/12 self-center">
-                        <Label htmlFor="date" value="date: " />
-                    </div>
-                    <div className="w-9/12 self-center">
-                        <TextInput id="date" type="datetime-local" value={date} onChange={onChangeDate} sizing="sm"/>
-                    </div>
-                </div>
-                <div className="flex flex-row h-12 text-end">
-                    <div className="w-2/12 self-center">
-                        <Label htmlFor="content" value="content: " />
-                    </div>
-                    <div className="w-9/12 self-center">
-                        <TextInput id="content" type="text" sizing="sm" value={content} onChange={onChangeContent}/>
-                    </div>
-                </div>
-                <div className="flex flex-row justify-end">
-                    <div className="w-9/12"></div>
-                    <div className="w-2/12">
-                        <Button color="gray" onClick={doPost}>작성</Button>
-                    </div>                    
-                </div>
-            </div>
-            </form>
-        </div>
+            </form >
+        </div >
     );
 }
