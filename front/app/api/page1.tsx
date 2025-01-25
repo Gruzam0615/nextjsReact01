5e52cb0860e224f0a0c9dd8712012ad83454147e
gruzam0615 <gruzam0615@gmail.com>
2025-01-25 10:33:45 +0900

    page1 list.map() list가 null 일 경우에 대한 렌더링 처리 적용

--- front/app/api/page1.tsx
+++ front/app/api/page1.tsx
@@ -1,5 +1,13 @@
 export async function getLists() {
-    const data = await fetch("https://api.vercel.app/blog")
-    const result = await data.json()
+    // const data = await fetch("https://api.vercel.app/blog")
+    // const result = await data.json()
+    // return result;
+    const result = await fetch("https://api.vercel.app/blog")
+    .then((res) => {
+        return res.json()
+    })
+    .catch((err) => {
+        return null;
+    })
     return result;
 }
